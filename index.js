3aaec13bdb2c8045675255def73f40210287ed7c
root <root@localhost.localdomain>
2018-03-08 21:32:55 -0500

    added user support

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+/*  index.js
+    server side Application of the chat app
+*/
+
+let express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var path = require('path');
+var available = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+var users = [];
+var User = class {
+  constructor(name, number){
+    this._name = name;
+    this._number = number;
+  }
+  get name(){
+    return this._name;
+  }
+  get number(){
+    return this._number;
+  }
+}
 
 app.get('/', function(req, res){
  res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -12,18 +30,39 @@ app.get('/', function(req, res){
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', function(socket){
-  console.log("New User.");
-
+  var currentUser = createUser();
+  socket.emit('user list', users);
+  console.log(currentUser.name + " Connected.");
+  socket.broadcast.emit('new user', currentUser.name);
   // Event handler for when a session ends.
   socket.on("disconnect", function() {
-    console.log("user disconnected.");
-    });
+    console.log("disconnect");
+    socket.broadcast.emit('exit user', currentUser.name);
+    available.push(currentUser.number);
+    console.log(currentUser.name + " disconnected.");
+    var index = users.indexOf(currentUser.name);
+    if (index > -1) {
+      users.splice(index, 1);
+    }
+  });
 
   socket.on('chat message', function(msg){
-    console.log("attempt to send :" + msg);
+    if (msg == "lol"){
+      for (i = 0; i < users.length; i++) {
+        console.log(users[i]);
+      }
+    }
     io.emit('chat message', msg);
   });
 });
+
+
+function createUser(){
+  var number = available.pop();
+  var newUser = new User("user" + number.toString(), number);
+  users.push(newUser.name);
+  return newUser;
+}
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
--- public/main.js
+++ public/main.js
@@ -4,6 +4,10 @@ $(document).ready(init);
 function init() {
   socket = io();
   socket.on('chat message', receiveMessage);
+  socket.on('user list', userList)
+  socket.on('new user', newUser);
+  socket.on('exit user', exitUser);
+
 }
 
 function sendMessage(){
@@ -16,3 +20,22 @@ function receiveMessage(msg){
   $('#messages').append($('<li>').text(msg));
   window.scrollTo(0, document.body.scrollHeight);
   }
+
+function userList(users){
+  for (i = 0; i < users.length; i++) {
+    $('#users').append($('<li>').text(users[i]));
+  }
+}
+
+function newUser(username){
+  $('#users').append($('<li>').text(username));
+}
+
+function exitUser(username){
+  for (let element of document.getElementById("users").childNodes){
+    if (element.innerText == username){
+      document.getElementById("users").removeChild(element);
+      return
+    }
+  }
+}
